81e47403ae54be6b9d635b4802ce531f1362127f
aaeeea <adrian.mikodaa@gmail.com>
2025-03-09 15:14:37 +0100

    first commit

---
+++ files/monte_carlo_method.c
@@ -0,0 +1,73 @@
+//approximation of the area in between the x axis and the sine function graph using the Monte Carlo method
+
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+double a,b;
+int n;
+
+struct point{
+    double x, y;
+};
+typedef struct point point;
+
+void input()
+{
+    printf("start of the interval a[pi]: ");
+    scanf("%lf", &a);
+
+    printf("end of the interval b[pi]: ");
+    scanf("%lf", &b);
+
+    printf("accuracy: ");
+    scanf("%d", &n);
+}
+
+point gen_random_point()
+{
+    point rand_point;
+
+    double rand_point_x = a+(b-a)*((double)rand()/RAND_MAX);
+    double rand_point_y = 2*((double)rand()/RAND_MAX)-1;
+
+    rand_point.x = rand_point_x*M_PI;
+    rand_point.y = rand_point_y;
+
+    return rand_point;
+}
+
+double monte_carlo()
+{
+    double area = (b-a)*2*M_PI;
+    int points_hit = 0;
+    for(int i = 0; i < n; i++){
+        point rand_point = gen_random_point();
+        double sin_val = sin(rand_point.x);
+
+        //checking if the point is between the x axis and the sine function graph
+        if(sin_val <= 0 && rand_point.y >= sin_val && rand_point.y <= 0){
+            points_hit++;
+        }
+        if(sin_val >= 0 && rand_point.y <= sin_val && rand_point.y >= 0)
+        {
+            points_hit++;
+        }
+    }
+    double x = (double)points_hit/n;
+    return(x*area);
+}
+
+int main(void)
+{
+    input();
+    if(b < a){
+        printf("Error: b must be greater or equal to a");
+    }
+    else{
+        srand(time(NULL)); //use time in seconds to set seed
+        printf("%lf", monte_carlo());
+    }
+    return 0;
+}
---
+++ files/taylor_series_cos.c
@@ -0,0 +1,58 @@
+//calculation of the value of the cosine function using the Taylor series expansion
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdio.h>
+
+double a,b,dx=1.0;
+int n;
+
+void input()
+{
+    printf("start of the interval a[pi]: ");
+    scanf("%f", &a);
+    printf("end of the interval b[pi]: ");
+    scanf("%f", &b);
+    printf("step size Dx[pi]: ");
+    scanf("%f", &dx);
+    printf("Taylor series accuracy (1-20): ");
+    scanf("%d", &n);
+}
+
+double cos_taylor_series(double x)
+{
+    double ans = 1;
+    double mult = -1;
+    long long factorial = 2;
+    double x_power = x*x;
+
+    for(int i =2; i<=n; i+=2){
+        ans += mult*x_power/factorial;
+        factorial *= (i+1)*(i+2);
+        x_power *= x*x;
+        mult *= -1;
+    }
+    return ans;
+}
+
+void calc()
+{
+    printf("\n%-10s %-20s %-20s\n", "x", "cos(x) (expected)", "cos(x) (Taylor series)");
+    for(float i = a; i <= b; i+=dx){
+        printf("%-10f %-20f %-20f\n", i, cos(i*M_PI), cos_taylor_series(i*M_PI));
+    }
+}
+
+int main(void)
+{
+    input();
+    if(b < a) {
+        printf("Error: b must be greater or equal to a");
+    }
+    else {
+        calc();
+    }
+    return 0;
+
+}
+
